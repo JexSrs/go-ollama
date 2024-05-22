8c8387e023837fe1840935ae6aa08fd72bc81acf
JexSrs <george@andredakis.gr>
2024-05-22 18:59:52 +0300

    Context fix

--- responses.go
+++ responses.go
@@ -2,18 +2,18 @@ package ollama
 
 // GCResponse represents the API response for "chat" and "generate" endpoints.
 type GCResponse struct {
-	Model              string   `json:"model"`
-	CreatedAt          string   `json:"created_at"`
-	Response           string   `json:"response"`
-	Message            Message  `json:"message"`
-	Done               bool     `json:"done"`
-	Context            []string `json:"context"`
-	TotalDuration      int64    `json:"total_duration"`
-	LoadDuration       int64    `json:"load_duration"`
-	PromptEvalCount    int64    `json:"prompt_eval_count"`
-	PromptEvalDuration int64    `json:"prompt_eval_duration"`
-	EvalCount          int64    `json:"eval_count"`
-	EvalDuration       int64    `json:"eval_duration"`
+	Model              string  `json:"model"`
+	CreatedAt          string  `json:"created_at"`
+	Response           string  `json:"response"`
+	Message            Message `json:"message"`
+	Done               bool    `json:"done"`
+	Context            []int   `json:"context"`
+	TotalDuration      int64   `json:"total_duration"`
+	LoadDuration       int64   `json:"load_duration"`
+	PromptEvalCount    int64   `json:"prompt_eval_count"`
+	PromptEvalDuration int64   `json:"prompt_eval_duration"`
+	EvalCount          int64   `json:"eval_count"`
+	EvalDuration       int64   `json:"eval_duration"`
 }
 
 // GenerateEmbeddingsResponse represents the API response for "generate embeddings" endpoint.
